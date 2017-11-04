324c4628ef55be618467f2cfa94c4f4f478a7239
Angela <angela.vu@bellevuecollege.edu>
2017-11-04 01:00:03 -0700

    Week06-WebCraftsJestStarter completed

---
+++ source/__tests__/sanity.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ReactHome from '../ReactHome';
+import HomeButtons from '../HomeButtons';
+import {configure, shallow} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import ElfDebugEnzyme from '../ElfDebugEnzyme';
+
+const elfDebugEnzyme = new ElfDebugEnzyme(true, 'sanity');
+configure({adapter: new Adapter()});
+import jQuery from 'jquery';
+
+global.jQuery = jQuery;
+global.$ = jQuery;
+//import '../fake-pub-sub';
+import {requestAnimationFrame} from '../temp-poly-fills';
+
+describe('WebCrafts Sanity Test', function () {
+
+    'use strict';
+
+    it('expects true to be true', function () {
+        expect(true).toBe(true);
+    });
+
+    it('ReactHome renders without crashing', () => {
+        const div = document.createElement('div');
+        ReactDOM.render(<ReactHome/>, div);
+    });
+
+    it('HomeButtons renders without crashing', () => {
+        const div = document.createElement('div');
+        ReactDOM.render(<HomeButtons/>, div);
+    });
+
+    it('renders default value of H1 tag', () => {
+        const wrapper = shallow(<ReactHome/>);
+        const h1tag = <h1>An H1 element in a React Component</h1>;
+        elfDebugEnzyme.getLast(wrapper, 'h1', true);
+        expect(wrapper.contains(h1tag)).toEqual(true);
+    });
+});
