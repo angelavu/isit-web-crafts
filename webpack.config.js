9a48f907d26d7d7831ada16e791b86f51521c516
Angela <angela.vu@bellevuecollege.edu>
2017-11-05 01:54:59 -0800

    Week07-ReactMakeHtml completed

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ var webpack = require('webpack');
 module.exports = {
     entry: './source/react-main.js',
     output: {path: __dirname, filename: 'bundle.js'},
+    devtool: "source-map",
     module: {
         loaders: [
             {
