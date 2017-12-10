45870627c66705a466870610a29d624af648640f
Angela <angela.vu@bellevuecollege.edu>
2017-12-09 22:33:31 -0800

    Final: Merge index to react-main, update layout with firebase

--- source/react-main.js
+++ source/react-main.js
@@ -1,32 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import ReactHome from './ReactHome';
-import MakeHtml from './MakeHtml'; //import MakeHtml main component
-import MakeImage from './MakeImage'; //import MakeImage main component
+import HomeButtons from './HomeButtons';
+import fireReducer from './fireReducer';
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
-let homeDiv = null;
+let store = createStore(fireReducer);
 
-function reactMakeHtml(event, customMessage) {
-    ReactDOM.render(<MakeHtml/>, homeDiv);
-}
+$(document).ready(function() {
+    try {
+        const root = document.getElementById('home');
+        ReactDOM.render(
 
-function reactMakeImage(event, customMessage) {
-    ReactDOM.render(<MakeImage/>, homeDiv);
-}
-
-function reactHome() {
-    document.getElementById('pageLoad').innerHTML = '';
-    home();
-}
-
-function home() {
-    ReactDOM.render(<div><ReactHome/></div>, homeDiv);
-}
-
-$(document).ready(function () {
-    homeDiv = document.getElementById('home');
-    $.subscribe('reactMakeHtml', reactMakeHtml);
-    $.subscribe('reactMakeImage', reactMakeImage);
-    $.subscribe('home', reactHome);
-    home();
+            <Provider store={store}>
+                <MuiThemeProvider>
+                    <div>
+                        <HomeButtons/>
+                    </div>
+                </MuiThemeProvider>
+            </Provider>
+            , root);
+    } catch (e) {
+        console.error(e);
+        document.getElementById('load').innerHTML = 'Error loading the Firebase SDK, check the console.';
+    }
 });
